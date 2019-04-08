7503c9f670e37353052fb61897ff6055031b91e4
Fox1986 <Liquid-Fox@web.de>
2019-04-08 09:25:39 +0200

    Registrierung hinzugefügt

--- index.php
+++ index.php
@@ -2,22 +2,42 @@
 <html>
 <head>
 	<title>Webchat</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div id="main">
 		<div id="info">
-			<h2>Login:</h2>
+			<h2>Anmelden:</h2>
 
 			<form action="login.php" method="post">
-			<label><b>Nutzername</b></label>
+			<label><b>Nutzername:</b></label>
 			<input type="text" name="uname" placeholder="User name"><br><br>
-			<label><b>Passwort</b></label>
+			<label><b>Passwort:</b></label>
 			<input type="text" name="pass" placeholder="Password"><br><br>
 			<button style="background-color: #6495ed; color: white" type="submit">
 				<b>Abschicken</b>
 			</button>
 			</form>
+
+			<form>
+				<form action="signup.php", method="post"></form>
+				<h2>Wenn Sie keinen Account haben Registrieren sich sie bitte hier</h2>
+				<label>Nutzername:</label>
+				<input type="text" name="uname", placeholder="Username"><br>
+				<br>
+				<label>E-Mail:</label>
+				<input type="text" name="email", placeholder="Email"><br>
+				<br>
+				<label>Passwort:</label>
+				<input type="text" name="Password", placeholder="Password"><br>
+				<br>
+				<button style="background-color: #6495ed; color: white" type="submit">
+				<b>Registrieren</b>
+			</button>
+			</form>
+
 		</div>
+
 	</div>
 
 </body>
