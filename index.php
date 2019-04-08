710dad949ad8a4a52e2cef31edf9380e8881083c
Fox1986 <Liquid-Fox@web.de>
2019-04-08 09:07:36 +0200

    Login in index.php erstellt

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webchat</title>
+</head>
+<body>
+	<div id="main">
+		<div id="info">
+			<h2>Login:</h2>
+
+			<form action="login.php" method="post">
+			<label><b>Nutzername</b></label>
+			<input type="text" name="uname" placeholder="User name"><br><br>
+			<label><b>Passwort</b></label>
+			<input type="text" name="pass" placeholder="Password"><br><br>
+			<button style="background-color: #6495ed; color: white" type="submit">
+				<b>Abschicken</b>
+			</button>
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
