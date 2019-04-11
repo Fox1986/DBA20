4a505fec2a3eff72fea25d2f47125ab7cac9804f
Fox1986 <Liquid-Fox@web.de>
2019-04-11 23:23:06 +0200

    Fehler6

--- index.php
+++ index.php
@@ -11,28 +11,28 @@
 
 			<form action="login.php" method="post">
 			<label><b>Nutzername:</b></label>
-			<input type="text" name="uname" placeholder="User name"><br><br>
+			<input type="text" name="nutzerkennung" placeholder="User name"><br><br>
 			<label><b>Passwort:</b></label>
-			<input type="text" name="pass" placeholder="Password"><br><br>
+			<input type="text" name="kennwort" placeholder="Password"><br><br>
 			<button style="background-color: #6495ed; color: white" type="submit">
 				<b>Abschicken</b>
 			</button>
 			</form>
 
-			<form>
-				<form action="signup.php", method="post"></form>
-				<h2>Wenn Sie keinen Account haben, Registrieren sich sie bitte hier:</h2>
-				<label>Nutzername:</label>
-				<input type="text" name="uname", placeholder="Username"><br>
-				<br>
-				<label>E-Mail:</label>
-				<input type="text" name="email", placeholder="Email"><br>
-				<br>
-				<label>Passwort:</label>
-				<input type="text" name="password", placeholder="Password"><br>
-				<br>
-				<button style="background-color: #6495ed; color: white" type="submit">
-				<b>Registrieren</b>
+			
+			<form action="registrieren.php", method="post">
+			<h2>Wenn Sie keinen Account haben, Registrieren sich sie bitte hier:</h2>
+			<label>Nutzername:</label>
+			<input type="text" name="uname", placeholder="Username"><br>
+			<br>
+			<label>E-Mail:</label>
+			<input type="text" name="email", placeholder="Email"><br>
+			<br>
+			<label>Passwort:</label>
+			<input type="text" name="password", placeholder="Password"><br>
+			<br>
+			<button style="background-color: #6495ed; color: white" type="submit">
+			<b>Registrieren</b>
 			</button>
 			</form>
 
--- login.php
+++ login.php
@@ -1,18 +1,16 @@
 <?php
+session_start();
 include 'datenbank.php';
-$uname=$_POST['uname'];
-$pass=$_POST['password'];
+$usname=$_POST['nutzerkennung'];
+$passd=$_POST['kennwort'];
 
-$sql="SELECT * FROM c_nutzer WHERE n_name='$uname' AND n_psswd='&pass'";
+$sql="SELECT * FROM `c_nutzer` WHERE n_name='$usname' AND n_psswd='&passd'";
 $result=$conn->query($sql);
 
 if(!$row=$result->fetch_assoc()){
 	header("Location:error.php");
 }else{
 	$_SESSION['name']=$_POST['uname'];
-
-
-	
 	header("Location:home.php");
 }
 
