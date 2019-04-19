4792e4c4cb3391fde6f96395a8a0173d66000eab
Fox1986 <Liquid-Fox@web.de>
2019-04-19 12:25:10 +0200

    änderung11

--- login.php
+++ login.php
@@ -12,11 +12,11 @@
 		$result = mysqli_query($conn,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		$active = $row['active'];
-		    
+		echo "5";  
 		$count = mysqli_num_rows($result);
 		      
 		      // If result matched $myusername and $mypassword, table row must be 1 row
-				
+		echo "6";		
 		if($count == 1) {
 		    session_register("myusername");
 		    $_SESSION['login_user'] = $myusername;
