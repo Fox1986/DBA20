44ba0840e4b34152286b71ef04db73ec16d7aa84
Fox1986 <Liquid-Fox@web.de>
2019-04-19 10:41:44 +0200

    login verändert

--- login.php
+++ login.php
@@ -1,17 +1,29 @@
 <?php
 session_start();
 include 'datenbank.php';
-$usname=$_POST['nutzerkennung'];
-$passd=$_POST['kennwort'];
 
-$sql="SELECT * FROM `c_nutzer` WHERE n_name='$usname' AND n_psswd='&passd'";
-$result=$conn->query($sql);
-
-if(!$row=$result->fetch_assoc()){
-	header("Location:error.php");
-}else{
-	$_SESSION['name']=$_POST['uname'];
-	header("Location:home.php");
-}
-
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($conn,$_POST['nutzerkennung']);
+      $mypassword = mysqli_real_escape_string($conn,$_POST['kennwort']); 
+      
+      $sql = "SELECT id FROM c_nutzer WHERE n_name = '$myusername' and n_psswd = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: home.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
 ?>
--- sess.php
+++ sess.php
@@ -2,7 +2,7 @@
 
 session_start();
 if(isset($_SESSION['name'])){
-	
+	echo $_SESSION['name'];
 }
 
 
